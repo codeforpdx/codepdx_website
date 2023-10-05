0197e3075b8dc9cffc39a12aad87f93be6a654e7
Andy <awilliamscomposing@gmail.com>
2023-10-04 23:11:45 -0700

    Replacing default css with theme.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// Theme Imports
+import theme from './theme';
 
 function App() {
   const [count, setCount] = useState(0)
