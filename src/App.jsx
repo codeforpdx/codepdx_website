109710f19b59c81eee5762290ff0121486821f31
veganedge <scottxbrown@yahoo.com>
2023-10-06 09:39:53 -0700

    skeleton of component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
-// React imports
-import { useState } from "react";
+// React Router Imports
+import { BrowserRouter } from "react-router-dom";
 // Material UI Imports
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 // Theme Imports
 import theme from "./theme";
+// Component Imports
+import Layout from "./Layout";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // any logic
 
   return (
     <>
       <CssBaseline />
       <ThemeProvider theme={theme}>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        <BrowserRouter>
+          <Layout />
+        </BrowserRouter>
       </ThemeProvider>
     </>
   );
---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,8 @@
+const Hero = () => {
+  // any logic needed
+  // return (
+  // all the JSX (looks like HTML) to render
+  // )
+};
+
+export default Hero;
