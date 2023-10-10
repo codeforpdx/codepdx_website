6c18c52e436ef5e62763023475fbb3cbc4bae213
Andy <awilliamscomposing@gmail.com>
2023-10-10 13:11:12 -0700

    Adjusting alignments

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -1,127 +1,103 @@
 // Material UI Imports
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 // import Divider from "@mui/material/Divider";
-import {
-  FaDiscord,
-  FaGithub,
-  FaLinkedin,
-  FaMeetup,
-} from "react-icons/fa";
-import Link from "@mui/material/Link";
-import LogoDevIcon from "@mui/icons-material/LogoDev";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
-import { useTheme } from "@mui/material/styles";
+import { FaDiscord, FaGithub, FaLinkedin, FaMeetup } from 'react-icons/fa6';
+import Link from '@mui/material/Link';
+import LogoDevIcon from '@mui/icons-material/LogoDev';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+// import { useTheme } from '@mui/material/styles';
 // Other Library Imports
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 
 // Legal links
 const legalLinks = [
   {
-    href: "https://www.codepdx.org/",
-    title: "About",
+    href: 'https://www.codepdx.org/',
+    title: 'About'
   },
   {
-    href: "https://www.codepdx.org/",
-    title: "Code of Conduct",
+    href: 'https://www.codepdx.org/',
+    title: 'Code of Conduct'
   },
   {
-    href: "https://www.codepdx.org/",
-    title: "Contact Us",
+    href: 'https://www.codepdx.org/',
+    title: 'Contact Us'
   },
   {
-    href: "https://www.codepdx.org/",
-    target: "_blank",
-    rel: "noopenner",
+    href: 'https://www.codepdx.org/',
+    target: '_blank',
+    rel: 'noopener',
     ml: 0.5,
     text: `©${dayjs().year()}`,
-    title: "CODE PDX",
-  },
+    title: 'CODE PDX'
+  }
 ];
 
 // Social media links
 const socialMediaLinks = [
   {
-    href: "https://github.com/codeforpdx/",
-    icon: <FaGithub fontSize="large" />,
-    target: "_blank",
-    rel: "noopenner",
+    href: 'https://github.com/codeforpdx/',
+    icon: <FaGithub size={40} />,
+    target: '_blank',
+    rel: 'noopener'
   },
   {
-    href: "https://www.linkedin.com/company/code-pdx/",
-    icon: <FaLinkedin fontSize="large" />,
-    target: "_blank",
-    rel: "noopenner",
+    href: 'https://www.linkedin.com/company/code-pdx/',
+    icon: <FaLinkedin size={40} />,
+    target: '_blank',
+    rel: 'noopener'
   },
   {
-    href: "https://www.meetup.com/Code-for-PDX/",
-    icon: <FaMeetup fontSize="large" />,
-    target: "_blank",
-    rel: "noopenner",
+    href: 'https://www.meetup.com/Code-for-PDX/',
+    icon: <FaMeetup size={40} />,
+    target: '_blank',
+    rel: 'noopener'
   },
   {
-    href: "https://discord.gg/x6b573et",
-    icon: <FaDiscord fontSize="large" />,
-    target: "_blank",
-    rel: "noopenner",
-  },
+    href: 'https://discord.gg/x6b573et',
+    icon: <FaDiscord size={40} />,
+    target: '_blank',
+    rel: 'noopener'
+  }
 ];
 
 const Footer = () => {
-  const theme = useTheme();
+  // const theme = useTheme();
 
   return (
     <Box
       component="footer"
       py={2}
       sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        position: "sticky",
-        top: "100%",
-        textAlign: "center",
-        bgcolor: "primary.main",
-        flexDirection: "column",
-        // minHeight: "100vh",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'sticky',
+        top: '100%',
+        textAlign: 'center',
+        bgcolor: 'primary.main',
+        flexDirection: 'column',
+        width: '100vw'
       }}
     >
       <Container
-      // maxWidth={"lg"}
-      // maxWidth={false}
-      // disableGutters
+      maxWidth={false}
       >
         <Stack
           alignItems="center"
-          direction={"row"}
+          direction={'row'}
           spacing={1}
-          // divider={
-          //   <Divider
-          //     // orientation={"horizontal"}
-          //     flexItem={null}
-          //     color={theme.palette.tertiary.main}
-          //     sx={{ height: "2px", width: 3 / 4 }}
-          //   />
-          // }
         >
           <Stack direction="row" alignItems="center">
-            <LogoDevIcon
-              fontSize="large"
-              // color="primary.main"
-            />
-            <Typography>CODE PDX</Typography>
+            <LogoDevIcon color="primary.main" />
+            <Typography variant="h5">CODE PDX</Typography>
           </Stack>
           <Box sx={{ flexGrow: 1 }} />
-          <Stack direction="row" spacing={3} alignItems="center">
+          <Stack direction="row" spacing={3} alignItems="center" sx={{ ml: '50px', mr: '50px' }}>
             {socialMediaLinks.map(({ href, icon }) => (
-              <Link
-                key={href}
-                href={href}
-                target="_blank"
-                rel="noopener"
-                color="#000"
-              >
+              <Link key={href} href={href} target="_blank" rel="noopener" color="#000">
                 {icon}
               </Link>
             ))}
@@ -133,6 +109,7 @@ const Footer = () => {
                 key={link.title}
                 variant="body2"
                 color="tertiary.main"
+                sx={{ display: 'flex', justifyContent: 'flex-end' }}
               >
                 {link.text ?? null}
                 <Link
