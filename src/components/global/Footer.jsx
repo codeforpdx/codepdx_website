044e5bb7de0533875d1a82ee14930df30ac0303a
Jared Krajewski <krajewski.jared@gmail.com>
2023-10-15 21:06:37 -0700

    fixed flexbox for narrow viewport

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -4,11 +4,13 @@ import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
 // Other Library Imports
 import dayjs from 'dayjs';
 import { FaDiscord, FaGithub, FaLinkedin, FaMeetup } from 'react-icons/fa6';
 // Image Imports
 import roseLogo from '/assets/rose_logo.png';
+import logoBlob from '/assets/logoBlob.svg';
 
 // Legal links
 const legalLinks = [
@@ -62,33 +64,6 @@ const socialMediaLinks = [
   }
 ];
 
-// maps legalLinks used for placement in stack below
-const renderLegalLinks = legalLinks.map((link) => (
-  <Typography
-    key={link.title}
-    variant="body2"
-    color="tertiary.main"
-    sx={{ display: 'flex', justifyContent: 'flex-end' }}
-  >
-    {link.text ?? null}
-    <Link
-      href={link.href}
-      underline="none"
-      color="#000"
-      target={link.target ?? null}
-      rel={link.rel ?? null}
-      ml={link.ml ?? null}
-      sx={{
-        '&:hover': {
-          color: 'primary.main'
-        }
-      }}
-    >
-      {link.title}
-    </Link>
-  </Typography>
-));
-
 // renders socialMediaLinks for placement in stack below
 const renderSocialLinks = socialMediaLinks.map(({ href, icon }) => (
   <Link
@@ -120,51 +95,112 @@ const socialBlobStyle = {
 const logoBlobStyle = {
   backgroundImage: 'url(/assets/logoBlob.svg)',
   backgroundRepeat: 'no-repeat',
-  // backgroundPosition: 'left',
-  overflow: 'visible'
-  // minWidth: '100vh',
-  // minHeight: '100vh'
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  overflow: 'visible',
+  minHeight: '40vh',
+  minWidth: '40vh'
 };
 
 const Footer = () => {
+  const theme = useTheme();
+
+  // maps legalLinks used for placement in stack below
+  const renderLegalLinks = legalLinks.map((link, index) => (
+    <Box key={link.title + index}>
+      <Typography
+        key={link.title}
+        variant="body2"
+        color="tertiary.main"
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          [theme.breakpoints.down('sm')]: {
+            p: '20px'
+          }
+        }}
+      >
+        {link.text ?? null}
+        <Link
+          href={link.href}
+          underline="none"
+          color="#000"
+          target={link.target ?? null}
+          rel={link.rel ?? null}
+          ml={link.ml ?? null}
+          sx={{
+            '&:hover': {
+              color: 'primary.main'
+            }
+          }}
+        >
+          {link.title}
+        </Link>
+      </Typography>
+    </Box>
+  ));
+
   return (
     // this box contains the entire footer
     <Box
       component="footer"
       sx={{
-        display: 'flex',
+        display: 'inline-flex',
         justifyContent: 'space-between',
         alignItems: 'center',
         position: 'fixed',
         bottom: 0,
         flexDirection: 'row',
         width: '100%',
-        background: 'lightgray'
+        background: 'lightgray',
+        [theme.breakpoints.down('sm')]: {
+          flexDirection: 'column'
+        }
       }}
     >
       <Stack direction="row" alignItems="center">
         <Box
-          component="img"
           sx={{
             marginRight: 2,
-            backgroundImage: 'url(/assets/logoBlob.svg)'
+            background: logoBlobStyle,
+            [theme.breakpoints.down('sm')]: {
+              p: '40px'
+            }
           }}
+          component="img"
           alt="CODE PDX logo"
           src={roseLogo}
         />
-        <Typography variant="h5">CODE PDX</Typography>
+        <Box>
+          <Typography variant="h5">CODE PDX</Typography>
+        </Box>
       </Stack>
       {/* <Box sx={{ flexGrow: 1 }} /> */}
       <Stack
         direction="row"
         spacing={3}
         alignItems="center"
-        sx={{ ml: '50px', mr: '50px', socialBlobStyle }}
+        sx={{
+          ml: '50px',
+          mr: '50px',
+          socialBlobStyle,
+          [theme.breakpoints.down('sm')]: {
+            p: '40px'
+          }
+        }}
       >
         {renderSocialLinks}
       </Stack>
       {/* <Box sx={{ flexGrow: 2 }} /> */}
-      <Stack>{renderLegalLinks}</Stack>
+      <Stack
+        sx={{
+          [theme.breakpoints.down('sm')]: {
+            p: '4vh'
+          }
+        }}
+      >
+        {renderLegalLinks}
+      </Stack>
     </Box>
   );
 };
