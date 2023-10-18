6974a83c766e198cdfbd343f63fb7ec48f04531a
Andy <awilliamscomposing@gmail.com>
2023-10-17 17:53:32 -0700

    Beginning abstraction and card styling

--- src/components/home/ProjectsBrief.jsx
+++ src/components/home/ProjectsBrief.jsx
@@ -1,27 +1,28 @@
 // Material UI Imports
 import Box from '@mui/material/Box';
-// import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
+import Chip from '@mui/material/Chip';
 import Grid from '@mui/material/Grid';
-// import Stack from '@mui/material/Stack';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 // Other Library Imports
-// import { FaDiscord, FaGithub, FaHome } from 'react-icons/fa6';
+import { FaDiscord, FaEarthAmericas, FaGithub } from 'react-icons/fa6';
 
 const projectsGrid = [
   {
     title: 'RecordSponge',
     status: 'Live',
     logo: '',
-    links: '',
-    techStack: ''
+    links: <FaGithub />,
+    techStack: 'Built with SOLID, React, Vite, JSDocs, MUI, NPM, ES Lint'
   },
   {
     title: 'PASS',
     status: 'In Development',
     logo: '',
-    links: '',
-    techStack: ''
+    links: <FaEarthAmericas />,
+    techStack: 'Built with SOLID, React, Vite, JSDocs, MUI, NPM, ES Lint'
   }
 ];
 
@@ -36,21 +37,17 @@ const ProjectsBrief = () => {
     >
       <Typography>Our Projects</Typography>
       <br />
-      <Card>
+      <Card sx={{ m: 10, p: 10 }}>
         <Grid container>
-          {projectsGrid.map(
-            ({
-              title,
-              status, logo, links, techStack
-            }) => (
-              <Grid item key={title}>
-                <Typography variant="body2">
-                  {title ?? null}
-                  {status ?? null}
-                </Typography>
-              </Grid>
-            )
-          )}
+          {projectsGrid.map(({ title, status, logo, links, techStack }) => (
+            <Grid item key={title}>
+              <Typography>{title ?? null}</Typography>
+              <Chip label={status} />
+              {logo ?? null}
+              {links ?? null}
+              <Typography variant="body2">{techStack ?? null}</Typography>
+            </Grid>
+          ))}
         </Grid>
       </Card>
     </Box>
