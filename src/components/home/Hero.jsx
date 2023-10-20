cf7af266352a8cfeea7ef9d4bb93eb741bb2cdb3
Ian O'Gorman <ogorman89@gmail.com>
2023-10-19 17:37:17 -0700

    add basic components to Hero

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,8 +1,39 @@
+// Material UI Imports
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 const Hero = () => {
-  // any logic needed
-  // return (
-  // all the JSX (looks like HTML) to render
-  // )
+  return (
+    <Box
+      sx={{
+        width: '100vw',
+        height: '40vh',
+        backgroundImage: 'url(/assets/stJohnsBridge.png)',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
+      <Typography variant="h1" component="h1">
+        Bridging Technology and Civil Services
+      </Typography>
+      <Button
+        variant="contained"
+        href="mailto:hugh@codeforpdx.org"
+        target="_blank"
+        rel="noopener"
+        sx={{
+          mt: '2rem',
+          width: 1 / 3,
+          '&:hover': {
+            color: 'white'
+          }
+        }}
+      >
+        Contact Us
+      </Button>
+    </Box>
+  );
 };
 
 export default Hero;
