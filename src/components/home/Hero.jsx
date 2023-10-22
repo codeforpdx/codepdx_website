d7e3d6fec2b792d7e732718bf79491da7d717526
Ian O'Gorman <ogorman89@gmail.com>
2023-10-22 10:13:09 -0700

    adjust sizing and scaling

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -4,39 +4,6 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 const Hero = () => {
-  const heroBackground = {
-    width: '100vw',
-    height: { xs: '30vh', md: '60vh' },
-    backgroundImage: 'url(/assets/stJohnsBridge.png)',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat'
-  };
-
-  const heroContainer = {
-    display: 'flex',
-    flexFlow: 'column',
-    flexBasis: 1,
-    justifyContent: 'flex-start',
-    alignItems: { xs: 'center', md: 'flex-start' },
-    width: { xs: '100%', md: 3 / 5 }
-  };
-
-  const h1Style = {
-    fontSize: { xs: '6vw', md: '6vw' },
-    pt: '1em',
-    pl: '0.75em',
-    width: { xs: '80%', md: '100%' },
-    textAlign: 'center'
-  };
-
-  const contactBtnStyle = {
-    alignSelf: 'center',
-    mt: '2em',
-    height: '2em',
-    width: { xs: '18vh', md: '17vh' }
-  };
-
   return (
     <Box sx={heroBackground}>
       <Box sx={heroContainer}>
@@ -55,6 +22,44 @@ const Hero = () => {
       </Box>
     </Box>
   );
+};
+
+const heroBackground = {
+  width: '100%',
+  height: { xs: '40vh', md: '50vh' },
+  backgroundImage: 'url(/assets/stJohnsBridge.png)',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat'
+};
+
+const heroContainer = {
+  display: 'flex',
+  flexFlow: 'column',
+  justifyContent: 'flex-start',
+  alignItems: { xs: 'center', md: 'flex-start' },
+  width: { xs: '100%', md: 3 / 5 },
+  height: '100%'
+};
+
+const h1Style = {
+  fontWeight: '400',
+  flexBasis: { xs: '100px', md: '200px' },
+  flexShrink: 1,
+  fontSize: 'calc(28px + 2.5vw)',
+  pt: '1em',
+  pl: { xs: 0, md: '0.75em' },
+  width: { xs: '80%', md: '100%' },
+  textAlign: 'center'
+};
+
+const contactBtnStyle = {
+  flexBasis: '30px',
+  flexShrink: 1,
+  alignSelf: 'center',
+  mt: '2em',
+  fontSize: 'calc(.5rem + .5vw)',
+  width: 'calc(6rem + 3vw)'
 };
 
 export default Hero;
