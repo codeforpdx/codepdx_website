82cd0718e5e6cf0b3ea33aa4794c3a68846cb513
Ian O'Gorman <ogorman89@gmail.com>
2023-10-22 11:05:42 -0700

    refactor to more closely match figma rendering on large screens

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
 
 const heroBackground = {
   width: '100%',
-  height: { xs: '40vh', md: '50vh' },
+  height: { xs: '50vh', md: '80vh' },
   backgroundImage: 'url(/assets/stJohnsBridge.png)',
   backgroundSize: 'cover',
   backgroundPosition: 'center',
@@ -38,28 +38,35 @@ const heroContainer = {
   flexFlow: 'column',
   justifyContent: 'flex-start',
   alignItems: { xs: 'center', md: 'flex-start' },
-  width: { xs: '100%', md: 3 / 5 },
+  width: { xs: '100%', md: '575px' },
   height: '100%'
 };
 
 const h1Style = {
   fontWeight: '400',
-  flexBasis: { xs: '100px', md: '200px' },
+  textShadow: '0px 4px 4px #0000004D',
+  lineHeight: { xs: '45px', sm: '80px' },
+  flexBasis: { xs: '100px', sm: '160px' },
   flexShrink: 1,
-  fontSize: 'calc(28px + 2.5vw)',
-  pt: '1em',
-  pl: { xs: 0, md: '0.75em' },
+  fontSize: { xs: '30px', sm: '56px' },
+  pt: { xs: '25px', sm: '50px', md: '113px' },
+  pl: { xs: 0, md: '56px' },
   width: { xs: '80%', md: '100%' },
   textAlign: 'center'
 };
 
 const contactBtnStyle = {
-  flexBasis: '30px',
+  textTransform: 'capitalize',
+  fontWeight: '200',
+  color: 'black',
+  textShadow: '0px 4px 4px #0000004D',
   flexShrink: 1,
   alignSelf: 'center',
   mt: '2em',
-  fontSize: 'calc(.5rem + .5vw)',
-  width: 'calc(6rem + 3vw)'
+  fontSize: { xs: '12px', sm: '15px', md: '18px' },
+  width: { xs: '103px', sm: '150px', md: '310px' },
+  height: { xs: '25px', sm: '50px', md: '84px' },
+  borderRadius: '5px'
 };
 
 export default Hero;
