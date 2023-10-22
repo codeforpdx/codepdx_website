6f7b3e7bf6b4e92b95d230a5af405f7f993ddd19
Ian O'Gorman <ogorman89@gmail.com>
2023-10-22 13:01:37 -0700

    change view height and remove unecessary breakpoints

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
 
 const heroBackground = {
   width: '100%',
-  height: { xs: '50vh', md: '80vh' },
+  height: { xs: '35vh', md: '70vh' },
   backgroundImage: 'url(/assets/stJohnsBridge.png)',
   backgroundSize: 'cover',
   backgroundPosition: 'center',
@@ -47,7 +47,6 @@ const h1Style = {
   textShadow: '0px 4px 4px #0000004D',
   lineHeight: { xs: '45px', sm: '80px' },
   flexBasis: { xs: '100px', sm: '160px' },
-  flexShrink: 1,
   fontSize: { xs: '30px', sm: '56px' },
   pt: { xs: '25px', sm: '50px', md: '113px' },
   pl: { xs: 0, md: '56px' },
@@ -60,12 +59,11 @@ const contactBtnStyle = {
   fontWeight: '200',
   color: 'black',
   textShadow: '0px 4px 4px #0000004D',
-  flexShrink: 1,
   alignSelf: 'center',
   mt: '2em',
-  fontSize: { xs: '12px', sm: '15px', md: '18px' },
-  width: { xs: '103px', sm: '150px', md: '310px' },
-  height: { xs: '25px', sm: '50px', md: '84px' },
+  fontSize: { xs: '15px', md: '18px' },
+  width: { xs: '115px', md: '310px' },
+  height: { xs: '25px', md: '84px' },
   borderRadius: '5px'
 };
 
