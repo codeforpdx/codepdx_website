b9d1a5e2fdab78740ff8f2d9a350b6e6c1822ec3
Jared Krajewski <krajewski.jared@gmail.com>
2023-10-25 14:59:04 -0700

    changed to exclude hero from container

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,23 +7,19 @@ import Navbar from './components/global/Navbar';
 import NoPageFound from './pages/NoPageFound';
 import Projects from './pages/Projects';
 import Volunteer from './pages/Volunteer';
-import { Container } from '@mui/material';
 
 const Layout = () => {
-  // any logic needed
   return (
     <>
       <Navbar />
-      <Container maxWidth="xl">
-        <main>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/volunteer" element={<Volunteer />} />
-            <Route path="*" element={<NoPageFound />} />
-          </Routes>
-        </main>
-      </Container>
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/volunteer" element={<Volunteer />} />
+          <Route path="*" element={<NoPageFound />} />
+        </Routes>
+      </main>
       <Footer />
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,22 @@
-import Hero from "../components/home/Hero";
-import About from "../components/home/About";
-import VolunteerBrief from "../components/home/VolunteerBrief";
-import Events from "../components/home/Events";
-import ProjectsBrief from "../components/home/ProjectsBrief";
-import Partners from "../components/home/partners/Partners";
+import Hero from '../components/home/Hero';
+import About from '../components/home/About';
+import VolunteerBrief from '../components/home/VolunteerBrief';
+import Events from '../components/home/Events';
+import ProjectsBrief from '../components/home/ProjectsBrief';
+import Partners from '../components/home/partners/Partners';
+import { Container } from '@mui/material';
 
 const Home = () => {
-  // any logic needed
   return (
     <>
       <Hero />
-      <About />
-      <VolunteerBrief />
-      <Events />
-      <ProjectsBrief />
-      <Partners />
+      <Container maxWidth="xl">
+        <About />
+        <VolunteerBrief />
+        <Events />
+        <ProjectsBrief />
+        <Partners />
+      </Container>
     </>
   );
 };
