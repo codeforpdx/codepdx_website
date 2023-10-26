7c4900f73dc08c1a04c5687699fe3ffb59bb7725
Jared Krajewski <krajewski.jared@gmail.com>
2023-10-26 16:55:50 -0700

    added hamburger menu and popover

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -1,7 +1,18 @@
-import { useState } from 'react';
-import { AppBar, Toolbar, Typography, Box, IconButton } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  Popover,
+  Toolbar,
+  Typography
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Brightness4, Brightness7 } from '@mui/icons-material';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Route, Link } from 'react-router-dom';
+import { useState, useRef } from 'react';
 
 const logoBlobStyle = {
   position: 'absolute',
@@ -26,22 +37,45 @@ const textStyle = {
 
 function NavBar() {
   const [darkMode, setDarkMode] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // anchor for menu to pop from its icon
+  const anchorRef = useRef(null);
 
   const handleThemeChange = () => {
     setDarkMode(!darkMode);
   };
 
+  const handleToggle = () => {
+    setMenuOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = () => {
+    setMenuOpen(false);
+  };
+
+  const menuItems = (
+    <List>
+      <ListItem component={Link} to="/" onClick={handleClose}>
+        Home
+      </ListItem>
+      <ListItem component={Link} to="/projects" onClick={handleClose}>
+        Projects
+      </ListItem>
+      <ListItem component={Link} to="/volunteer" onClick={handleClose}>
+        Volunteer
+      </ListItem>
+    </List>
+  );
+
   return (
     <AppBar position="static" color={darkMode ? 'dark' : 'primary'}>
       <Toolbar sx={{ height: '100px', alignItems: 'center' }}>
         <Box sx={logoBlobStyle}></Box>
-        <Link style={{ textDecoration: 'none' }} to="/">
+        <Link to="/" style={textStyle} aria-label="Home">
           <Box
-            position={'relative'}
             component="img"
             alt="CODE PDX logo"
-            aria-label="Code pdx"
-            zIndex={2}
             src={'/assets/rose_logo.png'}
             sx={{
               width: '75px',
@@ -50,24 +84,60 @@ function NavBar() {
             }}
           />
         </Link>
-        <Link to="/" style={textStyle}>
-          <Typography display={{ xs: 'none', sm: 'block' }} pl={'15px'} flex={1} variant="h4">
+        <Typography variant="h4" display={{ xs: 'none', sm: 'block' }} pl={'15px'}>
+          <Link to="/" style={textStyle} aria-label="Home">
             CODE PDX
-          </Typography>
-        </Link>
-        <Typography variant="body1" ml={'auto'} marginRight={2}>
-          <Link to="/projects" style={textStyle}>
+          </Link>
+        </Typography>
+
+        {/* Display for larger viewports */}
+        <Typography
+          variant="body1"
+          ml={'auto'}
+          display={{ xs: 'none', sm: 'block' }}
+          marginRight={2}
+        >
+          <Link to="/projects" style={textStyle} aria-label="Projects">
             Projects
           </Link>
         </Typography>
-        <Typography style={textStyle} variant="body1" marginRight={2}>
-          <Link to="/volunteer" style={textStyle}>
+        <Typography variant="body1" display={{ xs: 'none', sm: 'block' }} marginRight={2}>
+          <Link to="/volunteer" style={textStyle} aria-label="Volunteer">
             Volunteer
           </Link>
         </Typography>
+
+        {/* Hamburger menu for smaller viewports */}
+        <Hidden smUp>
+          <IconButton
+            sx={{ ml: 'auto' }}
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleToggle}
+            ref={anchorRef}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Hidden>
         <IconButton edge="end" color="inherit" onClick={handleThemeChange}>
           {darkMode ? <Brightness7 /> : <Brightness4 />}
         </IconButton>
+        <Popover
+          open={menuOpen}
+          anchorEl={anchorRef.current}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center'
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center'
+          }}
+        >
+          {menuItems}
+        </Popover>
       </Toolbar>
     </AppBar>
   );
