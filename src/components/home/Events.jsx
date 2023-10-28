ab494080b463bba5c893791d738e71586be9e01e
Jared Krajewski <krajewski.jared@gmail.com>
2023-10-28 12:27:14 -0700

    fixed imports

--- src/components/home/Events.jsx
+++ src/components/home/Events.jsx
@@ -1,4 +1,8 @@
-import { Container, Grid, Typography, Paper, Box } from '@mui/material';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 const leftSidePaperStyles = {
   elevation: 3,
