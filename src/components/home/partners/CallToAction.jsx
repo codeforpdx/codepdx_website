037dfccacc22d8463076ba7cd81d86631b579ff1
Jared Krajewski <krajewski.jared@gmail.com>
2023-10-29 22:26:22 -0700

    minor formatting

--- src/components/home/partners/CallToAction.jsx
+++ src/components/home/partners/CallToAction.jsx
@@ -1,22 +1,28 @@
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 const CallToAction = () => {
   return (
-    <Container maxWidth="lg" style={{ padding: '6rem 1rem' }}>
+    <Container maxWidth="xl" sx={{ pt: '100px', pb: '100px' }}>
       <Grid container spacing={1} alignItems="center">
         <Grid item xs={12} md={8}>
-          <Typography component="h4" variant="h4" pb={'2rem'}>
+          <Typography variant="h4" pb={'2rem'}>
             Empower Change: Become a partner in our civic tech movement.
           </Typography>
-          <Typography component="h4" variant="h4">
+          <Typography variant="h4">
             Have an idea for a civic project in Portland or want to partner with us?
           </Typography>
         </Grid>
         <Grid item xs={12} md={4} style={{ textAlign: 'center', alignItems: 'center' }}>
           <Button
+            sx={{
+              mt: {
+                xs: '50px',
+                md: '0'
+              }
+            }}
             variant="contained"
             color="primary"
             size="large"
