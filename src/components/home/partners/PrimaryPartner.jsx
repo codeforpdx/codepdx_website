8609d81b1f24d176aa627a531dc53f3f2770a245
Jared Krajewski <krajewski.jared@gmail.com>
2023-10-29 20:16:25 -0700

    flex styling for text

--- src/components/home/partners/PrimaryPartner.jsx
+++ src/components/home/partners/PrimaryPartner.jsx
@@ -4,10 +4,10 @@ import Typography from '@mui/material/Typography';
 
 const sponsorValues = {
   display: 'flex',
-  alignContent: 'center',
-  justifyContent: 'space-between',
-  height: '50%'
+  height: '50%',
+  padding: '5% 4% 50px 5%'
 };
+
 const PrimaryPartner = () => {
   return (
     <Container
@@ -29,24 +29,50 @@ const PrimaryPartner = () => {
         component={'h4'}
         variant="h4"
         textAlign={'center'}
-        padding={{ sm: '0 0 150px 0', md: '50px 200px 150px 200px' }}
+        padding={{ xs: '0 0 100px 0', md: '15px 15% 100px 15%' }}
       >
         Empowering businesses and entrepreneurs TAO is creating and uniting a world-class innovation
         economy in Oregon and beyond.
       </Typography>
       <Box
         sx={{
-          height: '400px',
-          mb: '100px'
+          height: 'auto',
+          pb: '100px'
         }}
       >
         <Box sx={sponsorValues}>
-          <Typography>blah blah</Typography>
-          <Typography>blah blah</Typography>
+          {/* top 2 values */}
+          <Typography variant="body1" sx={{ mr: '30%' }}>
+            Advocacy
+            <br />
+            <br />
+            We amplify your voice and bridge industry and government to drive policies that ensure
+            the growth and success of the region's tech industry.
+          </Typography>
+          <Typography variant="body1">
+            Community
+            <br />
+            <br />
+            We bring the regional tech community together through in-person and online events to
+            deepen relationships and build meaningful connections.
+          </Typography>
         </Box>
+        {/* bottom 2 values */}
         <Box sx={sponsorValues}>
-          <Typography>blah blah</Typography>
-          <Typography>blah blah</Typography>
+          <Typography variant="body1" sx={{ mr: '22%' }}>
+            Visibility
+            <br />
+            <br />
+            We shine a spotlight on the companies and people that power the region’s innovation
+            economy and leverage our platform to promote the local tech sector.
+          </Typography>
+          <Typography variant="body1">
+            Resources
+            <br />
+            <br />
+            We connect our members to resources to help their businesses grow and succeed through
+            public and private sector partnerships.
+          </Typography>
         </Box>
       </Box>
     </Container>
