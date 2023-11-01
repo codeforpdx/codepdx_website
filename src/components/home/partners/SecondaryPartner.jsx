bff4362abc1b1f11acd1a2b52c65d118bdc35026
Jared Krajewski <krajewski.jared@gmail.com>
2023-11-01 09:12:13 -0700

    abstracted partnerList and updated content

--- src/components/home/partners/SecondaryPartner.jsx
+++ src/components/home/partners/SecondaryPartner.jsx
@@ -3,36 +3,24 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
-const secondaryPartnerList = [
-  {
-    company: 'Open Commons',
-    testimonial:
-      'CODEPDXs exceptional team embodies talent and dedication, working relentlessly to enhance the lives of vulnerable citizens. Their innovative approach to utilizing technology for social betterment is truly remarkable.  - Wilfred Pinfold, President of OpenCommons',
-
-    partnerLogo: '/assets/openCommonsLogo.png'
-  },
-  {
-    company: 'ORDSN',
-    testimonial:
-      'By harnessing the diverse talents and enthusiasm of both tech-savvy and non-tech individuals, CODEPDX is carving a pathway to a more vibrant and inclusive Portland. Code for PDX exemplifies the very essence of what collaborative endeavors like ours at ORDSN aim to achieve. Siva Narendra, Co-Founder ORDSN Digital vaults are essential components of the future stack that every individual needs to control their lives in our modern economy.CODE PDXs focus on this exemplifies their understanding of what is most important for the vulnerable in our society. The remarkable team at Code for PDX should be applauded for their energy and vision. Carol Coye Benson, Co-Founder ORDSN',
-    partnerLogo: '/assets/ordsnLogo.png'
-  },
-  {
-    company: 'Epicodus',
-    testimonial:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quaerat saepe possimus ex dolor deleniti excepturi alias, est in tempore similique, inventore doloribus voluptatibus voluptate. Reiciendis minima nihil et dicta.',
-    partnerLogo: ''
-  }
-];
+import { secondaryPartnerList } from './secondaryPartnerList';
 
 const partnerGridStyle = {
   xs: 6,
   display: 'flex',
   justifyContent: 'center',
-  alignItems: 'center'
+  alignItems: 'center',
+  flexDirection: 'column'
 };
 
-const displaySecondaryPartners = ({ index, company, testimonial, partnerLogo }) => {
+const displaySecondaryPartners = ({
+  index,
+  company,
+  testimonial,
+  testimonialName,
+  partnerLogo,
+  website
+}) => {
   const gradientStyle =
     index % 2 === 0
       ? 'linear-gradient(90deg, rgba(217, 217, 217, 0) 38.54%, rgba(217, 217, 217, 0.4) 82.29%)'
@@ -47,23 +35,25 @@ const displaySecondaryPartners = ({ index, company, testimonial, partnerLogo }) 
         marginBottom: '150px'
       }}
       container
-      xs={12}
       key={company}
     >
       <Grid item {...partnerGridStyle}>
-        <Box
-          component={'img'}
-          alt={`${company} logo`}
-          aria-label={`${partnerLogo} logo`}
-          src={partnerLogo}
-          mb={'80px'}
-          width={'250px'}
-        ></Box>
+        <a href={website} target="_blank" rel="noopener noreferrer">
+          <Box
+            component={'img'}
+            alt={`${company} logo`}
+            aria-label={`${partnerLogo} logo`}
+            src={partnerLogo}
+            mb={'80px'}
+            width={'250px'}
+          ></Box>
+        </a>
       </Grid>
       <Grid item {...partnerGridStyle}>
-        <Typography variant="body1" display={'flex'} pr={'15%'}>
+        <Typography variant="body1" display={'flex'} p={'0 15% 3% 0'}>
           {testimonial}
         </Typography>
+        <Typography>{testimonialName}</Typography>
       </Grid>
     </Grid>
   );
---
+++ src/components/home/partners/secondaryPartnerList.jsx
@@ -0,0 +1,27 @@
+export const secondaryPartnerList = [
+  {
+    company: 'Open Commons',
+    testimonial:
+      "CODEPDX's exceptional team embodies talent and dedication, working relentlessly to enhance the lives of vulnerable citizens. Their innovative approach to utilizing technology for social betterment is truly remarkable.",
+    testimonialName: '- Wilfred Pinfold, President of OpenCommons',
+
+    partnerLogo: '/assets/openCommonsLogo.png',
+    website: 'https://opencommons.org/Main_Page'
+  },
+  {
+    company: 'ORDSN',
+    testimonial:
+      'By harnessing the diverse talents and enthusiasm of both tech-savvy and non-tech individuals, CODEPDX is carving a pathway to a more vibrant and inclusive Portland. Code for PDX exemplifies the very essence of what collaborative endeavors like ours at ORDSN aim to achieve.',
+    partnerLogo: '/assets/ordsnLogo.png',
+    website: 'https://www.ordsn.org/',
+    testimonialName: ' - Siva Narendra, Co-Founder ORDSN'
+  },
+  {
+    company: 'Epicodus',
+    testimonial:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quaerat saepe possimus ex dolor deleniti excepturi alias, est in tempore similique, inventore doloribus voluptatibus voluptate. Reiciendis minima nihil et dicta.',
+    partnerLogo: '/assets/epicodusLogo.png',
+    website: 'https://www.epicodus.com/',
+    testimonialName: '- someone important there'
+  }
+];
