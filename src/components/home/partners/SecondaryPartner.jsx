92d8426d2029a1fedb798e2da8b43b7f19fb5650
Jared Krajewski <krajewski.jared@gmail.com>
2023-11-02 09:12:33 -0700

    updated flex styling

--- src/components/home/partners/SecondaryPartner.jsx
+++ src/components/home/partners/SecondaryPartner.jsx
@@ -6,18 +6,22 @@ import Typography from '@mui/material/Typography';
 import { secondaryPartnerList } from './secondaryPartnerList';
 
 const partnerGridStyle = {
-  xs: 6,
+  xs: 12,
+  md: 6,
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  flexDirection: 'column'
+  flexDirection: 'column',
+  pt: { xs: '40px', md: 0 }
 };
 
 const displaySecondaryPartners = ({
   index,
   company,
   testimonial,
-  testimonialName,
+  testimonialTwo,
+  testimonialAuthorTwo,
+  testimonialAuthor,
   partnerLogo,
   website
 }) => {
@@ -26,34 +30,42 @@ const displaySecondaryPartners = ({
       ? 'linear-gradient(90deg, rgba(217, 217, 217, 0) 38.54%, rgba(217, 217, 217, 0.4) 82.29%)'
       : 'linear-gradient(270deg, rgba(217, 217, 217, 0) 38.54%, rgba(217, 217, 217, 0.4) 82.29%)';
 
+  const logoOrder = index % 2 === 1 ? 2 : 1;
+  const contentOrder = index % 2 === 1 ? 1 : 2;
+
   return (
     <Grid
+      container
       sx={{
         background: gradientStyle,
         borderRadius: '40px',
         minHeight: '500px',
-        marginBottom: '150px'
+        marginBottom: '100px',
+        flexDirection: { xs: 'column', md: 'row' }
       }}
-      container
       key={company}
     >
-      <Grid item {...partnerGridStyle}>
+      <Grid item xs={12} md={6} {...partnerGridStyle} order={logoOrder}>
         <a href={website} target="_blank" rel="noopener noreferrer">
           <Box
             component={'img'}
             alt={`${company} logo`}
             aria-label={`${partnerLogo} logo`}
             src={partnerLogo}
-            mb={'80px'}
-            width={'250px'}
+            mb={{ xs: '25px', md: '150px' }}
+            width={{ xs: '100px', md: '250px' }}
           ></Box>
         </a>
       </Grid>
-      <Grid item {...partnerGridStyle}>
-        <Typography variant="body1" display={'flex'} p={'0 15% 3% 0'}>
+      <Grid item pl={'6%'} {...partnerGridStyle} order={contentOrder}>
+        <Typography variant="body1" p={{ xs: '0 0 0 0 ', md: '0 15% 3% 0' }}>
           {testimonial}
         </Typography>
-        <Typography>{testimonialName}</Typography>
+        <Typography pb={'40px'}>{testimonialAuthor}</Typography>
+        <Typography variant="body1" display={'flex'} p={'0 15% 3% 0'}>
+          {testimonialTwo}
+        </Typography>
+        <Typography>{testimonialAuthorTwo}</Typography>
       </Grid>
     </Grid>
   );
