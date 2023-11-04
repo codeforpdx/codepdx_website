8d8a1f0642cc5d87f7453deac742b87c97726580
Jared Krajewski <krajewski.jared@gmail.com>
2023-11-04 12:13:25 -0700

    removed extra spaces

--- src/components/home/partners/SecondaryPartners.jsx
+++ src/components/home/partners/SecondaryPartners.jsx
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 import { secondaryPartnerList } from './secondaryPartnerList';
+
 import { PropTypes } from 'prop-types';
 
 const secondaryPartnerProps = {
