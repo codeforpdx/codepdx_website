f8cecd43638d2cbe5bf56c824e1f4dc720cc399c
Jared Krajewski <krajewski.jared@gmail.com>
2023-11-07 23:04:20 -0800

    trimmed and refactored code

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -3,119 +3,108 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
+// need to find out exact pictures to use
 const volunteerGrid = [
   {
     image:
       'https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8zMzI0NzU5Ni9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTczNzI1Nzg5MH0.PrIBXmpEeLrVe--JDq6kE2HF-DPuyzaLgfC1tnQ10Eo/img.jpg?width=742&quality=80',
-    label: '',
-    alt: 'kitten'
+    alt: 'Volunteer image one'
   },
   {
     image:
       'https://www.petmax.ca/cdn/shop/articles/tuqa-nabi-uhnZZUaTIOs-unsplash.jpg?v=1672753494',
-    label: '',
-    alt: 'kitten'
+    alt: 'Volunteer image two'
   },
   {
     image:
       'https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8zMzI0NzU5Ni9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTczNzI1Nzg5MH0.PrIBXmpEeLrVe--JDq6kE2HF-DPuyzaLgfC1tnQ10Eo/img.jpg?width=742&quality=80',
-    label: '',
-    alt: 'kitten'
+    alt: 'Volunteer image three'
   },
 
   {
     image:
       'https://media.istockphoto.com/id/1392163011/photo/cute-female-kitten-walking-towards-and-looking-in-the-camera-on-a-pink-background.jpg?s=612x612&w=0&k=20&c=y8qTH_C8CUOM17fVIxnpnTJljzsJaHMgfzGh6AZ2TwU=',
-    label: '',
-    alt: 'kitten'
+    alt: 'Volunteer image four'
   },
   {
     image:
       'https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8zMzI0NzU5Ni9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTczNzI1Nzg5MH0.PrIBXmpEeLrVe--JDq6kE2HF-DPuyzaLgfC1tnQ10Eo/img.jpg?width=742&quality=80',
-    label: '',
-    alt: 'kitten'
+    alt: 'Volunteer image five'
   }
 ];
 
-const renderTopImages = ({ image, label, alt }) => {
+const renderTopImages = ({ image, alt }) => {
   return (
     <Grid item key={alt}>
       <Box
         sx={{
-          display: 'inline-block',
           borderRadius: '1px',
-          overflow: 'hidden',
-          margin: '20px',
-          alignSelf: 'flex-end'
+          margin: '20px'
         }}
       >
         <img
-          src={image}
+          src={`${image}`}
           alt={alt}
           style={{ width: '200px', height: 'auto', borderRadius: '10px' }}
         />
       </Box>
-      <Typography variant="body2" align="center">
-        {label ?? null}
-      </Typography>
     </Grid>
   );
 };
 
-const renderBottomImages = ({ image, label, alt }) => {
+const TopImages = () => {
+  return (
+    <Grid container spacing={2} sx={{ justifyContent: 'center', alignItems: 'flex-end' }}>
+      {volunteerGrid.slice(0, 2).map(renderTopImages)}
+    </Grid>
+  );
+};
+
+const renderBottomImages = ({ image, alt }) => {
   return (
     <Grid item key={alt}>
-      <Box
-        sx={{ display: 'inline-block', borderRadius: '1px', overflow: 'hidden', margin: '20px' }}
-      >
-        <img
-          src={image}
-          alt={alt}
-          style={{ width: '200px', height: 'auto', borderRadius: '10px' }}
-        />
+      <Box sx={{ borderRadius: '1px', margin: '20px' }}>
+        <img src={image} alt={alt} style={{ width: '200px', borderRadius: '10px' }} />
       </Box>
-      <Typography variant="body2" align="center">
-        {label ?? null}
-      </Typography>
     </Grid>
   );
 };
 
+const BottomImages = () => {
+  return (
+    <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
+      {volunteerGrid.slice(2, 6).map(renderBottomImages)}
+    </Grid>
+  );
+};
 const About = () => {
   return (
-    <Grid container maxWidth={'xl'}>
+    <Grid container maxWidth={'xl'} pt={'5%'}>
       <Grid
         item
         xs={12}
-        s={12}
-        md={4}
+        md={5}
         sx={{
           display: 'flex',
           flexDirection: 'column',
-          alignItems: 'center',
-          border: '1px',
-          textAlign: 'center',
-          px: '0'
+          alignItems: 'center'
         }}
       >
-        <Typography variant="h3" component="h2" sx={{ px: '1rem', pt: '40px' }}>
+        <Typography variant="h3" component="h2" pt="40px" textAlign={'center'}>
           Who is CODE PDX?
         </Typography>
-        <Box sx={{ px: '2rem', textAlign: 'left', mt: '2rem' }}>
-          <Typography variant="h5" component="h3">
-            We are civic-minded residents committed to improving public services in Portland by
-            working with local government to utilize open data, improve citizen knowledge, and
-            implementing technological solutions. We are driven by our community for our community.
-          </Typography>
-        </Box>
+        <Typography variant="h5" component="h3" textAlign="center" p={'8% 0 8% 5%'}>
+          We are civic-minded residents committed to improving public services in Portland by
+          working with local government to utilize open data, improve citizen knowledge, and
+          implementing technological solutions. We are driven by our community for our community.
+        </Typography>
       </Grid>
-      <Grid item xs={12} s={12} md={8}>
-        <Grid container spacing={2} sx={{ justifyContent: 'center', alignItems: 'flex-end' }}>
-          {volunteerGrid.slice(0, 2).map(renderTopImages)}
-        </Grid>
-        <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
-          {volunteerGrid.slice(2, 6).map(renderBottomImages)}
-        </Grid>
+      {/* images */}
+      <Grid item xs={12} md={7}>
+        {/* top */}
+        <TopImages />
+        {/*  */}
+        <BottomImages />
       </Grid>
     </Grid>
   );
