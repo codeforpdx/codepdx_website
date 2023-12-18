9208d7961e364aea9ed5ba214ce83f9a3619f74b
veganedge <scottxbrown@yahoo.com>
2023-12-18 09:48:48 -0800

    icon size for mobile, gradient for mobile, center text for mobile, better conditional rendering

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -10,23 +10,20 @@ import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { useMediaQuery } from '@mui/material';
+// React Icons Import
+import { IconContext } from 'react-icons';
 // Custom Imports
 import Hero from '../components/global/Hero/Hero';
 import projectsList from '../components/projects/projectsList';
 
-const renderingLinks = (href, icon) => {
-  return (
-    <Link
-      component={ReactRouterLink}
-      key={href}
-      to={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      sx={linkStyle}
-    >
-      {icon}
-    </Link>
-  );
+const linkStyle = {
+  color: '#000',
+  textDecoration: 'none',
+  display: 'flex',
+  '&:hover': {
+    color: 'primary.main'
+  }
 };
 
 const boxStyle = (index) => ({
@@ -39,104 +36,101 @@ const boxStyle = (index) => ({
       index % 2 === 0
         ? 'linear-gradient(270deg, rgba(217, 217, 217, 0) 38.54%, rgba(217, 217, 217, 0.4) 82.29%)'
         : 'linear-gradient(90deg, rgba(217, 217, 217, 0) 38.54%, rgba(217, 217, 217, 0.4) 82.29%)',
-    xs: 'linear-gradient(0deg, rgba(217, 217, 217, 0) 38.54%, rgba(217, 217, 217, 0.4) 82.29%)'
+    xs: 'linear-gradient(0deg, rgba(217, 217, 217, 0) 18.54%, rgba(217, 217, 217, 0.4) 82.29%)'
   }
 });
 
-const linkStyle = {
-  color: '#000',
-  textDecoration: 'none',
-  display: 'flex',
-  '&:hover': {
-    color: 'primary.main'
-  }
-};
+const projectGridLogo = (index, title, logo, links, isSmallScreen) => {
+  const iconSize = isSmallScreen ? '30' : '45';
 
-const projectGridLogo = (index, title, logo, links) => {
   return (
     <Grid item xs={12} md={6} order={{ xs: 0, md: index % 2 === 0 ? 2 : 1 }}>
-      {/* Logo */}
-      <CardMedia
-        component="img"
-        image={logo ?? null}
-        alt={`${title} logo`}
-        sx={{
-          mx: 'auto',
-          objectFit: 'contain',
-          maxWidth: '90%',
-          height: '150px'
-        }}
-      />
-      {/* Logo ends */}
-      {/* Links */}
-      <Stack direction="row" justifyContent="space-evenly" pt={{ xs: 0, sm: '1rem' }}>
-        {links.map(({ href, icon }) => renderingLinks(href, icon))}
-      </Stack>
-      {/* Links end */}
+      {logo && (
+        <CardMedia
+          component="img"
+          image={logo}
+          alt={title ? `${title} logo` : 'project logo'}
+          sx={{
+            mx: 'auto',
+            objectFit: 'contain',
+            maxWidth: '90%',
+            height: '150px'
+          }}
+        />
+      )}
+      {links && (
+        <Stack direction="row" justifyContent="space-evenly" pt={{ xs: 0, sm: '1rem' }}>
+          <IconContext.Provider value={{ size: iconSize }}>
+            {links.map(({ href, icon }) => (
+              <Link
+                component={ReactRouterLink}
+                key={href}
+                to={href}
+                target="_blank"
+                rel="noopener noreferrer"
+                sx={linkStyle}
+              >
+                {icon}
+              </Link>
+            ))}
+          </IconContext.Provider>
+        </Stack>
+      )}
     </Grid>
   );
 };
 
-const projectGridContent = (index, description, title, status, techStack) => {
-  return (
-    <Grid maxWidth={'xl'} item xs={12} md={6} order={{ xs: 0, md: index % 2 === 0 ? 1 : 2 }}>
-      <CardContent>
-        {/* Title and status */}
-        <Stack
-          direction={{ xs: 'column', sm: 'row' }}
-          sx={{
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            mb: '2rem'
-          }}
-        >
+const projectGridContent = (index, description, title, status, techStack) => (
+  <Grid maxWidth={'xl'} item xs={12} md={6} order={{ xs: 0, md: index % 2 === 0 ? 1 : 2 }}>
+    <CardContent>
+      <Stack
+        direction={{ xs: 'column', sm: 'row' }}
+        sx={{
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          mb: '2rem'
+        }}
+      >
+        {title && (
           <Typography variant="h4" component="h3" sx={{ display: { xs: 'none', sm: 'block' } }}>
-            {title ?? null}
+            {title}
           </Typography>
+        )}
+        {status && (
           <Chip
-            label={status ?? null}
+            label={status}
             color="quaternary"
             variant="outlined"
             sx={{ backgroundColor: '#DEDEDEB2', fontWeight: 'bold' }}
           />
+        )}
+      </Stack>
+
+      <Divider />
+
+      {description && (
+        <Stack textAlign={{ xs: 'center', sm: 'left' }} my={2} spacing={2}>
+          <Typography variant="h6" component="h4" sx={{ fontWeight: 'bold' }}>
+            Overview
+          </Typography>
+          <Typography variant="body1">{description}</Typography>
         </Stack>
-        {/* Title and status end */}
-        <Divider />
-        {/* Content */}
-        <Typography
-          variant="h6"
-          component="h4"
-          sx={{
-            fontWeight: 'bold',
-            mb: 2,
-            mt: 3,
-            textAlign: { xs: 'center', sm: 'left' }
-          }}
-        >
-          Overview
-        </Typography>
-        <Typography variant="body1">{description ?? null}</Typography>
-        <Typography
-          variant="h6"
-          component="h4"
-          sx={{
-            fontWeight: 'bold',
-            my: 2,
-            textAlign: { xs: 'center', sm: 'left' }
-          }}
-        >
-          Technology Used
-        </Typography>
-        <Typography variant="body1" sx={{ textAlign: 'justify' }}>
-          {techStack ?? null}
-        </Typography>
-        {/* Content ends */}
-      </CardContent>
-    </Grid>
-  );
-};
+      )}
+      {techStack && (
+        <Stack textAlign={{ xs: 'center', sm: 'left' }} my={2} spacing={2}>
+          <Typography variant="h6" component="h4" sx={{ fontWeight: 'bold' }}>
+            Technology Used
+          </Typography>
+          <Typography variant="body1">{techStack}</Typography>
+        </Stack>
+      )}
+    </CardContent>
+  </Grid>
+);
 
 const Projects = () => {
+  const isSmallScreen = useMediaQuery('(max-width:600px)');
+
   return (
     <>
       <Hero
@@ -158,11 +152,11 @@ const Projects = () => {
           Our Projects
         </Typography>
         {projectsList
-          .slice(0, 2)
+          // .slice(0, 2)
           .map(({ index, description, title, status, logo, links, techStack }) => (
             <Box key={title} sx={boxStyle(index)}>
               <Grid container spacing={{ xs: 1, md: 10 }}>
-                {projectGridLogo(index, title, logo, links)}
+                {projectGridLogo(index, title, logo, links, isSmallScreen)}
                 {projectGridContent(index, description, title, status, techStack)}
               </Grid>
             </Box>
