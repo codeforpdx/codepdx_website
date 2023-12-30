b28e8bbc12a666ff5e95d15af28ae1c930d993b9
Jared Krajewski <krajewski.jared@gmail.com>
2023-12-29 22:21:16 -0800

    added 3 volunteer images, waiting on other two to submit

--- src/components/home/about/imageList.js
+++ src/components/home/about/imageList.js
@@ -1,29 +1,25 @@
-// need to replace with proper photos
 export const imageList = [
   {
-    image:
-      'https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8zMzI0NzU5Ni9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTczNzI1Nzg5MH0.PrIBXmpEeLrVe--JDq6kE2HF-DPuyzaLgfC1tnQ10Eo/img.jpg?width=742&quality=80',
-    alt: 'Volunteer image one'
-  },
-  {
-    image:
-      'https://www.petmax.ca/cdn/shop/articles/tuqa-nabi-uhnZZUaTIOs-unsplash.jpg?v=1672753494',
-    alt: 'Volunteer image two'
+    image: '/assets/volunteers/volunteer_hugh.webp',
+    alt: 'head shot of Hugh, image 1 out of 5'
   },
   {
     image:
       'https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8zMzI0NzU5Ni9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTczNzI1Nzg5MH0.PrIBXmpEeLrVe--JDq6kE2HF-DPuyzaLgfC1tnQ10Eo/img.jpg?width=742&quality=80',
-    alt: 'Volunteer image three'
+    alt: 'head shot of Ka Hung, image 2 out of 5'
+  },
+  {
+    image: '/assets/volunteers/volunteer_flo.webp',
+    alt: 'head shot of Flo, image 3 out of 5'
   },
 
   {
-    image:
-      'https://media.istockphoto.com/id/1392163011/photo/cute-female-kitten-walking-towards-and-looking-in-the-camera-on-a-pink-background.jpg?s=612x612&w=0&k=20&c=y8qTH_C8CUOM17fVIxnpnTJljzsJaHMgfzGh6AZ2TwU=',
-    alt: 'Volunteer image four'
+    image: '/assets/volunteers/volunteer_andy.webp',
+    alt: 'head shot of Andy, image 4 out of 5 '
   },
   {
     image:
       'https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8zMzI0NzU5Ni9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTczNzI1Nzg5MH0.PrIBXmpEeLrVe--JDq6kE2HF-DPuyzaLgfC1tnQ10Eo/img.jpg?width=742&quality=80',
-    alt: 'Volunteer image five'
+    alt: 'head shot of Scott, image 5 out of 5'
   }
 ];
