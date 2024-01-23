7696c3b222e088c2077bbecb050f54f98197e3de
Jared Krajewski <krajewski.jared@gmail.com>
2024-01-22 21:00:08 -0800

    removing comments and extra import

--- src/components/home/Events.jsx
+++ src/components/home/Events.jsx
@@ -1,5 +1,4 @@
 // Material UI Imports
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
@@ -25,8 +24,10 @@ const Events = () => {
           </Typography>
           <Paper {...leftSidePaperStyles}>
             <Typography p={2}>
-              CODE PDX meets up in-person once a month for a project demo night and onboarding
-              event. We also occasionally do social meetups.
+              CODE PDX meets up in-person twice a month at GRAYBOX in Portland. Once for a project
+              demo night and onboarding event and again for in person work session. We also
+              occasionally do non working social meetups. Event times and details can be found on
+              the calendar.
             </Typography>
           </Paper>
           <Paper
@@ -46,17 +47,14 @@ const Events = () => {
           </Paper>
           <Paper {...leftSidePaperStyles}></Paper>
         </Grid>
-        {/* box for calendar */}
         <Grid item xs={12} md={6}>
-          {/* will be google calendar I promise*/}
           <Typography variant="h3" align="center">
-            CODE pdx Event Calendar
+            CODE PDX Event Calendar
           </Typography>
           <iframe
             src="https://calendar.google.com/calendar/embed?src=ae96d1ee5deb4350a31a490321125fd21a0845e9ee921581ce27b925d5d707c5%40group.calendar.google.com&ctz=America%2FLos_Angeles&showTitle=0&showNav=0&showPrint=0&showTabs=0&showCalendars=0&src=YWU5NmQxZWU1ZGViNDM1MGEzMWE0OTAzMjExMjVmZDIxYTA4NDVlOWVlOTIxNTgxY2UyN2I5MjVkNWQ3MDdjNUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23A79B8E&color=%230B8043"
-            // style="border-width:0"
             width="100%"
-            height="550"
+            height="550px"
             frameBorder="0"
             scrolling="no"
           ></iframe>
