683b1c54e507ad0f2db060258197b2beed74eb1a
Jae <giffordjacob0@gmail.com>
2024-05-27 19:45:14 -0700

    projects brief blobs added with responsiveness

--- src/components/home/ProjectsBrief.jsx
+++ src/components/home/ProjectsBrief.jsx
@@ -8,12 +8,13 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import { useMediaQuery } from '@mui/material';
 // Component Imports
 import projectsList from '../projects/projectsList';
 // Theme
 import { useTheme } from '@emotion/react';
 
-const cardStyle = (theme) => {
+const cardStyle = (theme, isSmallScreen) => {
   return {
     display: 'flex',
     justifyContent: 'space-between',
@@ -25,13 +26,21 @@ const cardStyle = (theme) => {
     height: { xs: 'auto', sm: '100%' },
     border:
       theme.palette.mode === 'dark' ? `1px ${theme.palette.primary.boxOutline} solid` : 'none',
-    minHeight: { sm: '500px' }
+    minHeight: { sm: '500px' },
+    backgroundImage:
+      theme.palette.mode === 'dark'
+        ? 'url(/assets/partnerLogos/backgroundBlobs/mobileBlob.webp)'
+        : null,
+    backgroundRepeat: theme.palette.mode === 'dark' ? 'no-repeat' : null,
+    backgroundPosition: theme.palette.mode === 'dark' ? 'top' : null,
+    backgroundSize:
+      theme.palette.mode === 'dark' ? (isSmallScreen ? '120% 110px' : '120% 140px') : null
   };
 };
 
-const renderProjectBriefCard = (title, description, logo, theme) => (
+const renderProjectBriefCard = (title, description, logo, theme, isSmallScreen) => (
   <Grid key={title} item xs={12} md={6} lg={4}>
-    <Card sx={cardStyle(theme)}>
+    <Card sx={cardStyle(theme, isSmallScreen)}>
       <CardContent>
         {logo && (
           <CardMedia
@@ -70,6 +79,8 @@ const renderProjectBriefCard = (title, description, logo, theme) => (
 
 const ProjectsBrief = () => {
   const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <Box
       as="section"
@@ -81,7 +92,9 @@ const ProjectsBrief = () => {
         Our Projects
       </Typography>
       <Grid container rowSpacing={3}>
-        {projectsList.map((el) => renderProjectBriefCard(el.title, el.description, el.logo, theme))}
+        {projectsList.map((el) =>
+          renderProjectBriefCard(el.title, el.description, el.logo, theme, isSmallScreen)
+        )}
       </Grid>
     </Box>
   );
