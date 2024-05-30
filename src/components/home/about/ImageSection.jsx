c45ad08c49c994dd7b8aef37f3256473e5cfa7b5
Quan Zheng <q.zheng-7@student.tudelft.nl>
2024-05-29 20:49:06 -0700

    Randomize bottom image selection

--- src/components/home/about/ImageSection.jsx
+++ src/components/home/about/ImageSection.jsx
@@ -24,20 +24,28 @@ const TopImages = () => (
   </Grid>
 );
 
-const BottomImages = () => (
-  <Grid container spacing={4} sx={{ justifyContent: 'center', paddingTop: '20px' }}>
-    {imageList.slice(2).map(({ image, alt }) => (
-      <Grid item key={alt}>
-        <Box
-          component="img"
-          src={image}
-          alt={alt}
-          style={{ width: '200px', borderRadius: '10px' }}
-        />
-      </Grid>
-    ))}
-  </Grid>
-);
+const BottomImages = () => {
+  // Shuffle array
+  const shuffled = imageList.slice(2).sort(() => 0.5 - Math.random());
+
+  // Get sub-array of first n elements after shuffled
+  let selected = shuffled.slice(0, 3);
+
+  return (
+    <Grid container spacing={4} sx={{ justifyContent: 'center', paddingTop: '20px' }}>
+      {selected.map(({ image, alt }) => (
+        <Grid item key={alt}>
+          <Box
+            component="img"
+            src={image}
+            alt={alt}
+            style={{ width: '200px', borderRadius: '10px' }}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
 
 const ImageSection = () => (
   <Stack>
